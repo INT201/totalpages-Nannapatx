2c4f3d3088cd83b9e578e199e41f214c190d897f
Nannapatx <nannapat.it@mail.kmutt.ac.th>
2022-12-13 22:22:23 +0700

    Submit

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,15 @@
 const { template } = require('@babel/core')
-
+//64130500043 Nannapat Petcharach
 function totalPages(arrayItems, rowsPerPage) {
   //code here
+if(arrayItems === null || arrayItems === undefined){
+  return undefined
+}
+else if(rowsPerPage === null || rowsPerPage === undefined || rowsPerPage === 0){
+  return 1;
+}
+else{
+return Math.ceil(arrayItems.length/rowsPerPage)
+}
 }
 module.exports = totalPages
